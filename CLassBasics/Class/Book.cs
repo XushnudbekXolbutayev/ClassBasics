528ce98af974aa043f0dc67476cca5faca5c969d
XushnudbekXolbutayev <xushnudbekxolbutayev@gmail.com>
2023-12-28 16:48:34 +0500

    i have finished

---
+++ CLassBasics/Class/Book.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLassBasics.Class
+{
+    internal class Book
+    {
+        private string author;
+        public string name { get; set; }
+        public double orginalCost { get; set; }
+
+        public string Author
+        {
+            get
+            {
+                return author;
+            }
+            set
+            {
+                author = value;
+            }
+        }
+
+        public Book(string name, double presentCost, double profitCost)
+        {
+            orginalCost = presentCost - profitCost;
+            //bu shunchaki this kalit so'zini ishlatish uchun edi
+            this.name = name;
+        }
+
+        // params 
+        public static void Print(params object[] fullInfoBook)
+        {
+            for (int i = 0; i < fullInfoBook.Length; i++)
+            {
+                Console.WriteLine(fullInfoBook[i]);
+            }
+        }
+
+        // ref kalit so'zi
+        public static void Swapp(ref string name1, ref string name2)
+        {
+            string variable = name1;
+            name1 = name2;
+            name2 = variable;
+        }
+
+        // out kalit so'zi
+        public static void fullName(string frontWord, string backWord, out string Name)
+        {
+            Name = string.Concat(frontWord, backWord);
+        }
+    }
+}
---
+++ CLassBasics/Program.cs
@@ -0,0 +1,68 @@
+﻿using CLassBasics.Class;
+
+namespace CLassBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kitobning bozordagi narxi: ");
+            double costs = Convert.ToDouble(Console.ReadLine());
+
+            Book books = new Book("Harry Potter", costs, 12000);
+            books.Author = "J.K.Rowling";
+
+            Console.WriteLine($"Kitobning nomi: {books.name}");
+            Console.WriteLine($"Kitobning muallifi: {books.Author}");
+            Console.WriteLine($"Kitobning asl narxi: {books.orginalCost}");
+
+            // just for my learmimg
+            Console.WriteLine();
+
+            object[] objects = new object[] { "O'tkan kunlar", "Muallif", 45000 };
+            Print(objects);
+
+            Console.WriteLine();
+
+            string _name = " Hasan";
+            string _namee = "Husan";
+
+            Swapp(ref _name, ref _namee);
+
+            Console.WriteLine(_name + _namee);
+
+            Console.WriteLine();
+
+            string frontWord1 = "Harry";
+            string backWord1 = " Potter";
+            fullName(frontWord1, backWord1, out string Name);
+
+            Console.WriteLine(Name);
+
+        }
+
+        // params 
+        public static void Print(params object[] fullInfoBook)
+        {
+            for (int i = 0; i < fullInfoBook.Length; i++)
+            {
+                Console.WriteLine(fullInfoBook[i]);
+            }
+        }
+
+        // ref kalit so'zi
+        public static void Swapp(ref string name1, ref string name2)
+        {
+            string variable = name1;
+            name1 = name2;
+            name2 = variable;
+        }
+
+        // out kalit so'zi
+        public static void fullName(string frontWord, string backWord, out string Name)
+        {
+            Name = string.Concat(frontWord, backWord);
+        }
+    }
+    
+}
